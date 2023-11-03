8e7dc3cc456654cf3cae9ec33d7b91cec43eba86
LucaLF-dev <laficaraluca@gmail.com>
2023-11-03 16:37:18 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1> La mia prima app con Vite...</h1>
+  </div>
+</template>
+
+<style scoped>
+   h1 {
+    color: black ;
+    background-color: greenyellow;
+    padding: 20px;
+    border-top-left-radius: 30px;
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
+   }
+</style>
